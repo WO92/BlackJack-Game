ba3c9290445f86b7d2a35f19caf564fc7c2a60d0
Wallas The Mandalorian <144358282+WO92@users.noreply.github.com>
2024-07-21 20:57:51 +0200

    commit 1st
    test

---
+++ index.js
@@ -0,0 +1,33 @@
+let score = 0
+let score2 = 0;
+
+function addOne(){
+     score = score +1
+    document.getElementById("score").textContent= score
+}
+function addTwo(){
+     score = score +2
+    document.getElementById("score").textContent= score
+}
+function addThree(){
+     score = score +3
+    document.getElementById("score").textContent= score
+}
+function resetAll(){
+     score = 0
+     score2 = 0
+    document.getElementById("score").textContent= score 
+    document.getElementById("score2").textContent= score2
+}
+function addOne2(){
+     score2 = score2 +1
+    document.getElementById("score2").textContent= score2
+}
+function addTwo2(){
+     score2 = score2 +2
+    document.getElementById("score2").textContent= score2
+}
+function addThree2(){
+     score2 = score2 +3
+    document.getElementById("score2").textContent= score2
+}
